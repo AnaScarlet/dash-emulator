25b7d29c35de5897bb60631a430751d0089f9cbc
Anastasiya Lazarenko <laz1anastasiya@gmail.com>
2021-02-07 01:55:09 -0700

    Storing statistical data and fixing output() to match

--- dash_emulator/managers.py
+++ dash_emulator/managers.py
@@ -33,11 +33,12 @@ class PlayManager(object):
     class DownloadTaskSession(object):
         session_id = 1
 
-        def __init__(self, adaptation_set, url, next_task=None, duration: float = 0, representation_indices=None, segment_index: int = -1):
+        def __init__(self, adaptation_set, url, representation, next_task=None, duration: float = 0, representation_indices=None, segment_index: int = -1):
             self.session_id = PlayManager.DownloadTaskSession.session_id
             PlayManager.DownloadTaskSession.session_id += 1
             self.adaptation_set = adaptation_set  # type: mpd.AdaptationSet
             self.url = url
+            self.representation = representation
             self.next_task = next_task
             self.duration = duration
             self.representation_indices = representation_indices
@@ -76,9 +77,6 @@ class PlayManager(object):
 
             # The representations index of current playback. Key: adaptation set id, value: representation index
             self.representation_indices = dict()  # type: Dict[str, int]
-
-            # Statistical_data
-            self._bandwidth_segmentwise = {}
 
             # Download task sessions
             # type: Dict[str, PlayManager.DownloadTaskSession]
@@ -150,6 +148,7 @@ class PlayManager(object):
         segment_number = self.segment_index + representation.startNumber
         segment_download_session = PlayManager.DownloadTaskSession(adaptation_set,
                                                                    representation.urls[segment_number],
+                                                                   representation,
                                                                    None,
                                                                    representation.durations[segment_number],
                                                                    representation_indices=self.representation_indices,
@@ -157,6 +156,7 @@ class PlayManager(object):
         if not representation.is_inited:
             init_download_session = PlayManager.DownloadTaskSession(adaptation_set,
                                                                     representation.initialization_url,
+                                                                    representation,
                                                                     segment_download_session,
                                                                     0,
                                                                     representation_indices=self.representation_indices,
@@ -171,30 +171,31 @@ class PlayManager(object):
         Create download_task_sessions for current segment_index
         :return:
         """
-        representations = []
         for adaptation_set in self.mpd.adaptationSets.values():
             representation_index = self.representation_indices[adaptation_set.id]
             representation = adaptation_set.representations[representation_index]
             segment_number = self.segment_index + representation.startNumber
             segment_download_session = PlayManager.DownloadTaskSession(adaptation_set,
-                                                                       representation.urls[segment_number], None,
+                                                                       representation.urls[segment_number],
+                                                                       representation,
+                                                                       None,
                                                                        representation.durations[segment_number],
-                                                                       self.representation_indices
-                                                                       )
-            representations.append((representation.urls[segment_number].split('/')[-1], representation))  # Tuple(str, mpd.Representation)
+                                                                       self.representation_indices,
+                                                                       segment_index=self.segment_index)
 
             if not representation.is_inited:
                 init_download_session = PlayManager.DownloadTaskSession(adaptation_set,
                                                                         representation.initialization_url,
-                                                                        segment_download_session, 0,
-                                                                        self.representation_indices)
+                                                                        representation,
+                                                                        segment_download_session,
+                                                                        0,
+                                                                        self.representation_indices,
+                                                                        segment_index=self.segment_index)
                 representation.is_inited = True
                 self.download_task_sessions[adaptation_set.id] = init_download_session
             else:
                 self.download_task_sessions[adaptation_set.id] = segment_download_session
         
-        if len(DownloadManager().current_representations) == 0:
-            DownloadManager().current_representations = representations
 
     def init(self, cfg, mpd: mpd.MPD):
         self.cfg = cfg  # type: config.Config
@@ -290,12 +291,20 @@ class PlayManager(object):
             if len(self.download_task_sessions) == 0:
                 await events.EventBridge().trigger(events.Events.SegmentDownloadComplete,
                                                    segment_index=self.segment_index, duration=session.duration)
+                
+                # Record avg bandwidth for previous segment download
+                avg_bandwidth = monitor.SpeedMonitor().get_speed()
+                log.debug(f"Adding to avg bandwidth {avg_bandwidth} at [{self.segment_index}]")
+                DownloadManager().download_record[self.segment_index] = [avg_bandwidth, DownloadManager().current_representations]
+                DownloadManager().current_representations = []  # Reset current representations for next round
+
                 self.segment_index += 1
                 self.representation_indices = self.abr_controller.calculate_next_segment(
                     monitor.SpeedMonitor().get_speed(),
                     self.segment_index,
                     self.representation_indices,
                     self.mpd.adaptationSets)
+
                 try:
                     self.create_session_urls()
                 except IndexError as e:
@@ -344,8 +353,8 @@ class PlayManager(object):
             num_representatinons = len(DownloadManager().current_representations)
             for i in range(0, num_representatinons):
                 # Durations of segments
-                durations = DownloadManager().current_representations[i].durations
-                start_num = DownloadManager().current_representations[i].startNumber
+                durations = DownloadManager().current_representations[i][1].durations
+                start_num = DownloadManager().current_representations[i][1].startNumber
                 fig = plt.figure()
                 plt.plot([i for i in range(start_num, len(durations))], durations[start_num:])
                 plt.xlabel("Segments")
@@ -356,8 +365,8 @@ class PlayManager(object):
 
                 # Download bandwidth of each segment
                 fig = plt.figure()
-                inds = [i for i in sorted(self._bandwidth_segmentwise.keys())]
-                bws = [self._bandwidth_segmentwise[i] for i in inds]
+                inds = [i for i in sorted(DownloadManager()._bandwidth_segmentwise.keys())]
+                bws = [DownloadManager()._bandwidth_segmentwise[i] for i in inds]
                 plt.plot(inds, bws)
                 plt.xlabel("Segments")
                 plt.ylabel("Bandwidth (bps)")
@@ -397,56 +406,75 @@ class PlayManager(object):
             output_path = self.cfg.args['output']
 
             # Reports
-            seg_inds = [i for i in sorted(self._bandwidth_segmentwise.keys())]
-            bws = [self._bandwidth_segmentwise[i] for i in seg_inds]
+            dr = DownloadManager().download_record
             with open(os.path.join(output_path, 'results.csv'), 'w') as f:
-                writer = csv.DictWriter(f, fieldnames=["index", "filename", "init_name", "avg_bandwidth", "bitrate",
-                                                       "width", "height", "mime", "codec"])
+                writer = csv.DictWriter(f, fieldnames=["segment_index", "avg_bandwidth", "filename", "id", "bitrate",
+                                                       "width", "height", "mime", "codec", "bandwidth_difference"])
                 writer.writeheader()
-                for (segment_name, representation), ind, bw in zip(DownloadManager().download_record, seg_inds, bws):
-                    record = {
-                        "index": ind,
-                        "filename": segment_name,
-                        "init_name": representation.init_filename,
-                        "avg_bandwidth": bw,
-                        "bitrate": representation.bandwidth,
-                        "width": representation.width,
-                        "height": representation.height,
-                        "mime": representation.mime,
-                        "codec": representation.codec
+                for ind in dr.keys():
+                    bws = dr[ind][0]
+                    representation_tuples = dr[ind][1]
+                    record1 = {
+                        "segment_index" : ind,
+                        "avg_bandwidth" : bws
                     }
-                    writer.writerow(record)
+                    writer.writerow(record1)
+                    bws_sum = 0
+                    bws_avg_counter = 0
+                    for r_tuple in representation_tuples:
+                        tile_name = r_tuple[0]
+                        representation = r_tuple[1]
+                        bws_sum += representation.bandwidth
+                        bws_avg_counter += 1
+                        record2 = {
+                            "filename": tile_name,
+                            "id": representation.id,
+                            "bitrate": representation.bandwidth,
+                            "width": representation.width,
+                            "height": representation.height,
+                            "mime": representation.mime,
+                            "codec": representation.codec
+                        }
+                        writer.writerow(record2)
+
+                    theoretical_avg_bandwidth = bws_sum / bws_avg_counter
+                    bandwidth_difference = bws - theoretical_avg_bandwidth
+                    record_avg = {
+                        "bitrate" : theoretical_avg_bandwidth,
+                        "bandwidth_difference" : bandwidth_difference
+                    }
+                    writer.writerow(record_avg)
 
                 # Merge segments into a complete one
-                tmp_output_dir_name = '-playback-merge-%05d' % random.randint(1, 99999)
-                #with tempfile.TemporaryDirectory(suffix=tmp_output_dir_name) as tmp_output_path:
-                #os.mkdir("temp")
-                tmp_output_path = "temp"
-                log.info(f'Creating temporary directory: {tmp_output_path}')
-                os.mkdir(tmp_output_path)
-                segment_list_file_name = 'segment-list.txt'
-                segment_list_file_path = os.path.join(tmp_output_path, segment_list_file_name)
-                log.info(f'Creating temporary file: {segment_list_file_path}')
-                target_output_path = "%s/%s" % (output_path, 'playback.mp4')
+                # tmp_output_dir_name = '-playback-merge-%05d' % random.randint(1, 99999)
+                # with tempfile.TemporaryDirectory(suffix=tmp_output_dir_name) as tmp_output_path:
+                #     # tmp_output_path = "temp"
+                #     # os.mkdir(tmp_output_path)
+                #     log.info(f'Creating temporary directory: {tmp_output_path}')
+                #     segment_list_file_name = 'segment-list.txt'
+                #     segment_list_file_path = os.path.join(tmp_output_path, segment_list_file_name)
+                #     log.info(f'Creating temporary file: {segment_list_file_path}')
+                #     target_output_path = "%s/%s" % (output_path, 'playback.mp4')
 
-                for (segment_name, representation), ind, bw in zip(DownloadManager().download_record, seg_inds, bws):
-                    with open('%s/%s' % (output_path, 'merge-segment-%d.mp4' % ind), 'wb') as f:
-                        subprocess.call(['cat', f"{output_path}{os.path.sep}{representation.init_filename}",
-                                            f"{output_path}{os.path.sep}{segment_name}"], stdout=f)
-                    tmp_segment_path = f'{tmp_output_path}{os.path.sep}segment-{ind}.mp4'
-                    subprocess.call(
-                        ['ffmpeg', '-i', f"{output_path}{os.path.sep}merge-segment-{ind}.mp4", '-vcodec', 'libx264',
-                         '-vf', 'scale=1920:1080', tmp_segment_path], stdout=subprocess.DEVNULL,
-                        stderr=subprocess.DEVNULL)
-                    with open(segment_list_file_path, 'a') as f:
-                        subprocess.call(['echo', f'file {tmp_segment_path}'], stdout=f)
-                        f.flush()
+                #     for (segment_name, representation), ind, bw in zip(DownloadManager().download_record, seg_inds, bws):
+                #         with open('%s/%s' % (output_path, 'merge-segment-%d.mp4' % ind), 'wb') as f:
+                #             subprocess.call(['cat', f"{output_path}{os.path.sep}{representation.init_filename}",
+                #                                 f"{output_path}{os.path.sep}{segment_name}"], stdout=f)
+                #         tmp_segment_path = f'{tmp_output_path}{os.path.sep}segment-{ind}.mp4'
+                #         subprocess.call(
+                #             ['ffmpeg', '-i', f"{output_path}{os.path.sep}merge-segment-{ind}.mp4", '-vcodec', 'libx264',
+                #             '-vf', 'scale=1920:1080', tmp_segment_path], stdout=subprocess.DEVNULL,
+                #             stderr=subprocess.DEVNULL)
+                #         with open(segment_list_file_path, 'a') as f:
+                #             subprocess.call(['echo', f'file {tmp_segment_path}'], stdout=f)
+                #             f.flush()
 
-            print(target_output_path)
-            subprocess.call(
-                ['ffmpeg', '-f', 'concat', '-safe', '0', '-i',
-                    segment_list_file_path, '-c', 'copy', target_output_path],
-                stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+            # Merge segments into a complete one
+            # print(target_output_path)
+            # subprocess.call(
+            #     ['ffmpeg', '-f', 'concat', '-safe', '0', '-i',
+            #         segment_list_file_path, '-c', 'copy', target_output_path],
+            #     stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
 
         if self.cfg.args['output'] is not None:
             events.EventBridge().add_listener(events.Events.End, output)
@@ -477,7 +505,9 @@ class DownloadManager(object):
             # This property records all downloaded segments and its corresponding representation
             # Each tuple in the list represents a segment
             # Each tuple contains 2 elements: filename of the segment, corresponding representation
-            self.download_record = []  # type: List[Tuple[str, mpd.Representation]]
+            #TODO: change this to a 2D list of tples: List[segment_index, bandwidth, List[Tuple[str, mpd.Representation]]]
+                                        # Might as well merge this and _bandwidth_segmentwise? Or, move _bandwidth_segmentwise back into the PlayManager
+            self.download_record = {}  # type: {"segment_index" : List["avg_bandwidth", List[Tuple[str, mpd.Representation]]}
             self.current_representations = []  # type: List[MPD.Representation]
 
     async def record_buffer_level(self, *args, **kwargs):
@@ -500,10 +530,6 @@ class DownloadManager(object):
             csvLogger.propagate = False
 
         csvLogger.info('\n\n**Results**\n\n')
-
-        #csvLogger.info("Download record:")
-        #for r in DownloadManager().download_record:
-        #    csvLogger.info(f"Representation: \t {r[0]} \t id: {r[1].id} \t urls: {r[1].urls}")
 
         # logging 'percentage downloaded' statistics for all tile-segments
         csvLogger.info('Percentage of bytes received for each tile:\n')
@@ -538,6 +564,15 @@ class DownloadManager(object):
             csvLogger.info(f'{average},\t')
         overall_average = float(cum_sum / num_tiles)
         csvLogger.info(f'\t\t {overall_average}\n')
+
+        csvLogger.info("\nDownload record:")
+        dr = DownloadManager().download_record
+        for ind in dr.keys():
+           csvLogger.info(f"Segment Index: {ind} \t Avg bandwidth: {dr[ind][0]} \n")
+           for r_tuple in dr[ind][1]:
+               name = r_tuple[0]
+               r = r_tuple[1]
+               csvLogger.info(f"Representation: \t name: {name} \t id: {r.id} \t url: {r.baseurl} \t bandwidth: {r.bandwidth}")
 
     async def download(self, url, download_progress_monitor) -> None:
         """
@@ -634,13 +669,9 @@ class DownloadManager(object):
                 monitor_task = loop.create_task(
                     download_progress_monitor.start())
 
-            # Add segment to download record
-            self.download_record.extend(self.current_representations)
 
-            # print("Updated Download record:")
-            # for r in self.download_record:
-            #     print(r[0], r[1].tag, r[1].attrib)
-
+            self.current_representations.append( (session.url.split('/')[-1], session.representation) )
+            
             try:
                 await task
             except asyncio.CancelledError:
